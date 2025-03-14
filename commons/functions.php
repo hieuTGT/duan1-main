296f4bf81d256ba211849fd3147298719ec68b08
hieuTGT <166585712+hieuTGT@users.noreply.github.com>
2025-03-14 08:26:26 +0700

    Update functions.php

--- commons/functions.php
+++ commons/functions.php
@@ -18,28 +18,6 @@ function connection()
     }
 }
 
-// Render view client
-function view($view, $data = [])
-{
-    extract($data);
-    include_once "views/$view.php";
-}
 
-// Render view admin
-function view_admin($view, $data = [])
-{
-    extract($data);
-    include_once "../admin/views_admin/$view.php";
-}
 
-// Chuyển đổi trạng thái đơn hàng
-function getOrderStatus($status) {
-    $status_details = [
-        1 => 'Chờ xử lý',
-        2 => 'Đang xử lý',
-        3 => 'Hoàn  thành',
-        4 => 'Đã hủy'
-    ];
-    return $status_details[$status];
-}
 ?>
